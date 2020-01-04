b22e483149445cf7aecd94d95e717175e9ab8ede
Geoffrey Brown <gbrown@brodio.com>
2020-01-03 22:18:22 -0500

    updated comments

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
+// grab the value of the countdown ID
 let seconds = document.getElementById("countdown").textContent;
+
+// iterate down from the countdown ID value
 let countdown = setInterval(() => {
     seconds--;
     document.getElementById("countdown").textContent = seconds;
     if (seconds <= 0) clearInterval(countdown);
 }, 1000);
-
-/* function myInterval(i) {
-        setTimeout(() => console.log("step = " + i), 1000);
-}
-
-for (let i = 0; i < 5; i++) {
-    myInterval(i);
-} */
